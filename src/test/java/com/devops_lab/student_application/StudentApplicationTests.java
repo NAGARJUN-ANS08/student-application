3ffd54189e242a64e15b569a902a7779b06f55a2
Nagarjun <nagarjunmyuniversity08@gmail.com>
2026-09-18 13:45:23 +0530

    Java Application Initialised

---
+++ src/test/java/com/devops_lab/student_application/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops_lab.student_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
